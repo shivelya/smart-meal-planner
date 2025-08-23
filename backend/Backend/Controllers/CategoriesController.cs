841ebd297e60fa08586df0e09eee2b73471e6b93
acochran <ashley.cochran.m@gmail.com>
2025-08-23 16:03:41 -0400

    Putting the category controller behind an Authroize attribute.

--- backend/Backend/Controllers/CategoriesController.cs
+++ backend/Backend/Controllers/CategoriesController.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Backend.DTOs;
 using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Backend.Controllers
 {
     /// <summary>
-    /// Lists all category types
+    /// Lists all category types. They are currently static.
     /// </summary>
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class CategoriesController : ControllerBase
@@ -19,6 +21,10 @@ namespace Backend.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Lists all category types. They are currently static.
+        /// </summary>
+        /// <returns>All the existing category types.</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories()
         {
