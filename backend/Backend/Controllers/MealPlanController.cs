171b5c9b550e6b907855566deeeede3bb34a1d52
acochran <ashley.cochran.m@gmail.com>
2025-09-11 13:30:43 -0400

    Renaming controller to its default since that's how I left the others.

--- backend/Backend/Controllers/MealPlanController.cs
+++ backend/Backend/Controllers/MealPlanController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Backend.Controllers
 {
     [ApiController]
-    [Route("api/meal-plan")]
+    [Route("api/[controller]")]
     [Authorize]
     public class MealPlanController(IMealPlanService service, ILogger<MealPlanController> logger, IConfiguration configuration) : ControllerBase
     {
