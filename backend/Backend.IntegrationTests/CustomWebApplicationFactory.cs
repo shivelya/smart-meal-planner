801b4692585c224cf2890106a71407b290acefa0
acochran <ashley.cochran.m@gmail.com>
2025-09-18 16:39:31 -0400

    Adding seed data and the ability to log in.

--- backend/Backend.IntegrationTests/CustomWebApplicationFactory.cs
+++ backend/Backend.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,3 +1,7 @@
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Backend.DTOs;
+using Backend.Model;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
@@ -32,7 +36,54 @@ namespace Backend.IntegrationTests
                 {
                     options.UseNpgsql(_databaseContainer.GetConnectionString());
                 });
+
+                using var scope = services.BuildServiceProvider().CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<PlannerContext>();
+                db.Database.EnsureCreated();
+
+                db.Users.Add(new User { Email = "test@example.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("password") });
+
+                db.Foods.Add(new Food { CategoryId = 1, Name = "Bananas" });
+                db.Foods.Add(new Food { CategoryId = 1, Name = "Spaghetti" });
+                db.Foods.Add(new Food { CategoryId = 2, Name = "Fried Chicken" });
+                db.Foods.Add(new Food { CategoryId = 3, Name = "Dumplings" });
+
+                db.SaveChanges();
+
+                db.Recipes.Add(new Recipe
+                {
+                    Source = "Spoonacular - 636603",
+                    UserId = 1,
+                    Title = "Butternut Squash Soup with Fresh Goat Cheese",
+                    Instructions = "Peel and remove seeds from the squash cut in large 2 inch pieces.\nPlace squash in heavy gauge sauce pot and cover with the milk and water.\nBring to a boil and simmer for 20 minutes until very tender.\nAdd 1 package Chavrie Goat Cheese Pyramid and bring back to a simmer.\nRemove from the heat.\nCarefully remove squash with a slotted spoon and place in blender. Add enough of the cooking liquid to cover the squash puree in the blender. Be very careful: you must leave the center cap of the blender off so you do not trap the steam (cover loosely with a towel).\nPour into a 1 gallon sauce pot and keep warm.\nReserve and repeat until all the squash has been pureed.\nAdjust to desired consistency with the remaining cooking liquid. Season with salt and pepper.\nTo serve, ladle the hot soup into the individual soup bowls and top with a dollop of Chavrie Goat Cheese Pyramid garnish with fresh herbs.",
+                    Ingredients = [
+                        new RecipeIngredient { Food = new Food { CategoryId = 1, Name = "Goat Cheere"}, Quantity = 10.6M, Unit = "oz" },
+                            new RecipeIngredient { Food = new Food { CategoryId = 1, Name = "Butternut Squash"}, Quantity = 1 },
+                            new RecipeIngredient { Food = new Food { CategoryId = 1, Name = "water" }, Quantity = 0.5M, Unit = "qt" },
+                            new RecipeIngredient { Food = new Food { CategoryId = 1, Name = "Milk"}, Quantity = 0.5M, Unit = "qt" }
+                    ]
+                });
+
+                db.PantryItems.Add(new PantryItem { FoodId = 1, UserId = 1, Quantity = 3, Unit = "bunches" });
+                db.PantryItems.Add(new PantryItem { FoodId = 3, UserId = 1, Quantity = 1, Unit = "bag" });
+                db.PantryItems.Add(new PantryItem { FoodId = 6, Quantity = 12, UserId = 1 });
+
+                db.SaveChanges();
             });
+        }
+
+        public async Task<TokenResponse> Login(HttpClient client)
+        {
+            var loginResponse = await client.PostAsJsonAsync("/api/auth/login", new LoginRequest
+            {
+                Email = "test@example.com",
+                Password = "password"
+            });
+
+            loginResponse.EnsureSuccessStatusCode();
+            var result = await loginResponse.Content.ReadFromJsonAsync<TokenResponse>();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result!.AccessToken);
+            return result;
         }
     }
 }
