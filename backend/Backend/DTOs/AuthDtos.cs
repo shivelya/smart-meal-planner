5bcee993507bdd395b24728955fc6aa12433b2c9
acochran <ashley.cochran.m@gmail.com>
2025-09-18 16:38:40 -0400

    Marking the Email property as such so it is verified for formatting.

--- backend/Backend/DTOs/AuthDtos.cs
+++ backend/Backend/DTOs/AuthDtos.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.DTOs
 {
     public class LoginRequest
     {
+        [EmailAddress]
         public required string Email { get; set; }
         public required string Password { get; set; }
     }
